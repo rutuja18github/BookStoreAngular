e936aa3eced12da6cf40b46ddb4fc401cb4eb010
RutujaPatil18 <prutuja189@gmail.com>
2023-01-16 19:58:03 +0530

    [Rutuja]Added :Create user login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './Components/signup/signup.component';
+import { SigninComponent } from './Components/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
