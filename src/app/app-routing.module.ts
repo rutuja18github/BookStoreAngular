0e3c3da831f1e333fd0726e96d09d0e8f01852ba
RutujaPatil18 <prutuja189@gmail.com>
2023-01-20 00:33:09 +0530

    [Rutuja]Added:Create place order page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CartComponent } from './Components/cart/cart.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { DisplayBookComponent } from './Components/display-book/display-book.component';
 import { OpenBookViewComponent } from './Components/open-book-view/open-book-view.component';
+import { PlaceOrderPageComponent } from './Components/place-order-page/place-order-page.component';
 import { SigninComponent } from './Components/signin/signin.component';
 import { SignupComponent } from './Components/signup/signup.component';
 import { WishlistComponent } from './Components/wishlist/wishlist.component';
@@ -17,7 +18,9 @@ const routes: Routes = [
     {path:'display',component:DisplayBookComponent},
     {path:'openBook',component:OpenBookViewComponent},
     {path:'cart',component:CartComponent},
-    {path: 'wishlist', component:WishlistComponent},
+    {path: 'placeOrder', component:PlaceOrderPageComponent},
+    {path: 'wishlist', component:WishlistComponent}
+    
   ]}
 ];
 
